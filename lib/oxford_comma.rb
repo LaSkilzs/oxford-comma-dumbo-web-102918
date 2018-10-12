c944870690209bc5f61f2737b3be000817f17bf6
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-12 23:13:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
  return array.join if array.count == 1
  
  if array.count > 1 
-   array.last = "and " + array.last
+   array[-1] = "and " + array[-1]
    array = array.join(",")
  end
   array
